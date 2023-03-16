2eb2b906d537d082e7969c3d1c3559d4ad0aa7b4
MahmoudEasa <mu01011422865@gmail.com>
2023-03-16 12:28:28 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pointer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	return (1);
+}
