882bd0b78e72abb8292ea38a3e65ca51194076d7
MahmoudEasa <mu01011422865@gmail.com>
2023-03-17 09:16:44 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alwys (0) [Success]
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar((char)num);
+		num++;
+	}
+	putchar((char)10);
+
+	return (0);
+}
