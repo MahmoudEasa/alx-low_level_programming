4c3fe1859aeb58c0aa2bdffd5324cd7f7a06037f
MahmoudEasa <mu01011422865@gmail.com>
2023-03-17 07:13:51 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) [Secces]
+ */
+
+int main(void)
+{
+	int c_lower = (int)'a', c_upper = (int)'A';
+
+	while (c_lower <= (int)'z')
+	{
+		putchar((char)c_lower);
+		c_lower++;
+	}
+	while (c_upper <= (int)'Z')
+	{
+		putchar((char)c_upper);
+		c_upper++;
+	}
+
+	putchar((char)10);
+
+	return (0);
+}
