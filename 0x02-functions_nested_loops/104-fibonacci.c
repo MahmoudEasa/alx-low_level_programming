d6618180e92307f4c54df604920246613482916d
MahmoudEasa <mu01011422865@gmail.com>
2023-03-22 08:26:31 -0700

    Fix 104-fibonacci.c 5

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,26 +8,42 @@
 
 int main(void)
 {
-	long double num1 = 0, num2 = 0, temp = 0;
+	unsigned long n1 = 0, n2 = 1, temp;
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+	unsigned long h1, h2;
 	int i;
 
-	for (i = 1; i <= 98; i++)
+	for (i = 0; i <= 92; i++)
 	{
-		if (i > 2)
+		temp = n1 + n2;
+		printf("%lu, ", temp);
+		n1 = n2;
+		n2 = temp;
+	}
+
+	n1_h1 = n1 / 10000000000;
+	n2_h1 = n2 / 10000000000;
+	n1_h2 = n1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2_h2;
+
+		if ((n1_h2 + n2_h2) > 9999999999)
 		{
-			temp = num1;
-			num1 += num2;
-			num2 = temp;
+			h1 += 1;
+			h2 %= 10000000000;
 		}
-		else
-		{
-			num1 = i;
-			num2 = i - 1;
-		}
-		printf("%.0Lf", num1);
-		if (i == 98)
-			continue;
-		printf(", ");
+		printf("%lu%lu", h1, h2);
+		if (i != 98)
+			printf(", ");
+
+		n1_h1 = n2_h1;
+		n1_h2 = n2_h2;
+		n2_h1 = h1;
+		n2_h2 = h2;
 	}
 	printf("\n");
 	return (0);
