688c97bf0639af18ca085be80ad81dc3a6894ec0
MahmoudEasa <mu01011422865@gmail.com>
2023-03-23 12:17:20 -0700

    Fix 5-more_numbers.c 1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
-			_putchar(j + '0');
-		for (j = 0; j <= 4; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar('1');
-			_putchar(j + '0');
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
