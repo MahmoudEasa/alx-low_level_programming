801bc45843aba4a1ae58880175ad45febddca87f
MahmoudEasa <mu01011422865@gmail.com>
2023-03-23 16:14:25 -0700

    Fix 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,42 +7,18 @@
 
 void print_number(int n)
 {
-	int one, ten, hundred;
+	unsigned int num = n;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		_putchar(-(n / 10) + '0');
-		_putchar(-(n % 10) + '0');
+		num = -n;
 	}
-	else if (n < 100)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n < 1000)
-	{
-		one = n / 100;
-		ten = n / 10;
 
-		_putchar((one) + '0');
-		_putchar((ten % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
+	if (num > 9)
 	{
-		one = n / 1000;
-		ten = n / 100;
-		hundred = n % 100;
-
-		_putchar((one) + '0');
-		_putchar((ten % 10) + '0');
-		_putchar((hundred / 10) + '0');
-		_putchar((n % 10) + '0');
+		print_number(num / 10);
 	}
+	_putchar(num % 10 + '0');
 }
 
