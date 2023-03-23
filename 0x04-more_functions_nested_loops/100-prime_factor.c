c955ba42a96d2d324241ac7a57ee52f9f28ad312
MahmoudEasa <mu01011422865@gmail.com>
2023-03-23 14:56:05 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	int i;
+
+	while (n % 2 == 0)
+	{
+		printf("%d\n", 2);
+		n = n / 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			printf("%d\n", i);
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		printf("%ld\n", n);
+	return (0);
+}
+
