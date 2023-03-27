411d4b8b3b3fdbcb9c1656e81a88ef61807107e5
MahmoudEasa <mu01011422865@gmail.com>
2023-03-27 10:40:33 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -8,10 +8,14 @@
  */
 int main(void)
 {
-    char s[10] = "My School";
+	char s[10] = "My School";
+	char s1[11]= "Holberton!";
 
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	printf("%s\n", s1);
+	rev_string(s1);
+	printf("%s\n", s1);
+	return (0);
 }
