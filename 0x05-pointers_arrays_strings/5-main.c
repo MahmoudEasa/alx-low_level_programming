c2e265b387f6ed82ce2f6c98ffb6542548960aaf
MahmoudEasa <mu01011422865@gmail.com>
2023-03-27 11:32:47 -0700

    Fix 5-rev_string.c 2

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	char s[10] = "My School";
 	char s1[11]= "Holberton!";
+	char s2[446] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 	printf("%s\n", s);
 	rev_string(s);
@@ -17,5 +18,8 @@ int main(void)
 	printf("%s\n", s1);
 	rev_string(s1);
 	printf("%s\n", s1);
+	printf("%s\n", s2);
+	rev_string(s2);
+	printf("%s\n", s2);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,49 +7,17 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
-	int length = _strlen(s) - 1;
+	int len = 0, i = 0;
+	char tmp;
 
-	while (i < length)
-	{
-		swap_char(s + length, s + i);
-		i++;
-		length--;
-	}
-}
-
-/**
- * _strlen - return the length of a string
- * @s: (char) Pointer of string
- *
- * Return: Length of s
- */
-
-int _strlen(char *s)
-{
-	int len;
-	char *ptr = s;
-
-	while (*ptr != '\0')
-	{
+	while (s[len] != '\0')
 		len++;
-		ptr++;
+
+	while (i < len--)
+	{
+		tmp = s[i];
+		s[i++] = s[len];
+		s[len] = tmp;
 	}
-
-	return (len);
-}
-
-/**
- * swap_char - swap to characters
- * @a: first character
- * @b: second character
- */
-
-void swap_char(char *a, char *b)
-{
-	char temp = *a;
-
-	*a = *b;
-	*b = temp;
 }
 
