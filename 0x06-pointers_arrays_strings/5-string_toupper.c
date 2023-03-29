cf721052fbb30ca49f35247ca06da2f76085d7da
MahmoudEasa <mu01011422865@gmail.com>
2023-03-29 16:40:46 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string
+ *
+ * Return: pointer to uppercase string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) = *(s + i) - 32;
+		i++;
+	}
+
+	return (s);
+}
+
