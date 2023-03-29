90312b075bff0e3f0cfed4409d3e6de46522ae8a
MahmoudEasa <mu01011422865@gmail.com>
2023-03-29 15:07:33 -0700

    Fix 3-strcmp.c 1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,27 +13,45 @@ int _strlen(char *s);
 
 int _strcmp(char *s1, char *s2)
 {
-	unsigned int i = 0, n1 = 0, n2 = 0;
+	unsigned int i = 0, n1 = 0, n2 = 0, is_strcmp = 0;
+	unsigned int s1_len = _strlen(s1);
+	unsigned int s2_len = _strlen(s2);
 
-	while (s1[i++] != '\0')
-		n1 += s1[i];
-
-	i = 0;
-
-	while (s2[i++] != '\0')
-		n2 += s2[i];
-
-	if (n1 < n2)
+	if (s1_len != s2_len)
 	{
-		return (-15);
-	}
-	else if (n1 > n2)
-	{
-		return (15);
+		while (s1[i++] != '\0')
+			n1 += s1[i];
+
+		i = 0;
+
+		while (s2[i++] != '\0')
+			n2 += s2[i];
+
+		if (n1 < n2)
+		{
+			is_strcmp = -15;
+		}
+		else
+			is_strcmp = 15;
 	}
 	else
-		return (0);
+	{
+		while (s1[i++] != '\0')
+		{
+			if (s1[i] < s2[i])
+			{
+				is_strcmp = -15;
+				break;
+			}
+			else if (s1[i] > s2[i])
+			{
+				is_strcmp = 15;
+				break;
+			}
+		}
+	}
 
+	return (is_strcmp);
 }
 
 /**
