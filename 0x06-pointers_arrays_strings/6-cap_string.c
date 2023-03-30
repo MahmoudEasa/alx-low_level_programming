1d94e7b1184a3499eafb99b82218455ad541e376
MahmoudEasa <mu01011422865@gmail.com>
2023-03-29 17:35:48 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string
+ *
+ * Return: pointer to string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 32;
+
+		while (1)
+		{
+			if (*(s + i) == '-')
+			{
+				i++;
+				continue;
+			}
+
+			if (*(s + i) == '\0' || *(s + i) == '\n'
+				       || (*(s + i) >= 32 && *(s + i) <= 47))
+				break;
+
+			if (*(s + i) == '\t')
+			{
+				*(s + i) = ' ';
+				break;
+			}
+
+			i++;
+		}
+
+		if (*(s + i) == '\0')
+			break;
+		i++;
+	}
+
+	return (s);
+}
+
