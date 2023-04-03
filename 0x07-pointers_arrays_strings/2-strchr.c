f82769ca3337d1e0f88bd885af3c6f365013058f
MahmoudEasa <mu01011422865@gmail.com>
2023-04-03 10:37:38 -0700

    Fix 2-strchr.c 1

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,17 +12,16 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
-	char *f = NULL;
 
 	while (*(s + i) != '\0')
 	{
 		if (c == *(s + i))
 		{
-			f = (s + i);
-			break;
+			return (s + i);
 		}
+
 		i++;
 	}
-	return (f);
+	return (NULL);
 }
 
