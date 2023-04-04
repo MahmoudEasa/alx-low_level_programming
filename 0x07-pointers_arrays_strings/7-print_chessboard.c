5bbecdc9b3d31b980fc36a242f8db8ed989f3775
MahmoudEasa <mu01011422865@gmail.com>
2023-04-04 04:32:44 -0700

    Fix 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,11 +14,7 @@ void print_chessboard(char (*a)[8])
 	for (j = 0; j < 8; j++)
 	{
 		for (i = 0; i < 8; i++)
-		{
-			if (a[j][i] == ' ')
-				break;
 			_putchar(a[j][i]);
-		}
 		_putchar(10);
 	}
 }
