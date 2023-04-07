858241747e346f71c0e650dc3edfd889e1a7388e
MahmoudEasa <mu01011422865@gmail.com>
2023-04-07 07:45:27 -0700

    Fix 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,10 @@
  * Return: 0
  */
 
-int main(int argc,__attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", (argc - 1));
+	if (argv[argc - 1])
+		printf("%d\n", (argc - 1));
 
 	return (0);
 }
