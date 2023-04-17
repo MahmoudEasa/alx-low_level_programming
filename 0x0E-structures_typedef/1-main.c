318f20e90aa9d80364e51711bc2e0bf3bcd9dd29
MahmoudEasa <mu01011422865@gmail.com>
2023-04-17 09:15:58 -0700

    Fix 1-init_dog.c

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -12,5 +12,6 @@ int main(void)
 
     init_dog(&my_dog, "Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    printf("My name is %s, and I am %.1f, and owner: %s :) - Woof!\n", my_dog.name, my_dog.age, my_dog.owner);
     return (0);
 }
