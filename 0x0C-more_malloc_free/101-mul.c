69fb9cca987465d2f482c8c492c3d24c1f6012c5
MahmoudEasa <mu01011422865@gmail.com>
2023-04-18 14:50:50 -0700

    Fix 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,28 +5,6 @@
 #include <ctype.h>
 #include <limits.h>
 
-typedef struct multiplynode
-{
-	char data;
-	struct multiplynode *next;
-	struct multiplynode *prev;
-}MultiplyNode;
-
-typedef struct myltiply
-{
-	int size;
-	MultiplyNode *top;
-	MultiplyNode *end;
-}Multiply;
-
-int _isdigit(char *s);
-void create_node(Multiply *pm);
-void _push(char c, Multiply *pm);
-void multiply_nums(char *arg1, char *arg2,
-	Multiply *pm, MultiplyNode *p_h, int s1_len, int s2_len);
-void multiply_first_num(char *arg1, char *arg2, Multiply *pm,
-	int s1_len, int s2_len);
-
 /**
  * main - multiplies two positive numbers
  * @argc: count of arguments
@@ -38,9 +16,8 @@ void multiply_first_num(char *arg1, char *arg2, Multiply *pm,
 int main(int argc, char **argv)
 {
 	char *arg1, *arg2;
-	int s1_len, s2_len, i;
+	int s1_len, s2_len;
 	Multiply m;
-	MultiplyNode *p_h = NULL;
 
 	if ((argc != 3) || (!_isdigit(argv[1]) || !_isdigit(argv[2])))
 	{
@@ -53,43 +30,21 @@ int main(int argc, char **argv)
 	s1_len = strlen(arg1);
 	s2_len = strlen(arg2);
 
-	/*if the tow numbers <= 9999 multiply them
+	/* if the tow numbers <= 9999 multiply them */
 	if (s1_len <= 4 && s2_len <= 4)
 	{
 		printf("%d\n", (atoi(arg1) * atoi(arg2)));
 		return (0);
-	}*/
+	}
 
 	/*Create Node*/
 	create_node(&m);
 
 	/*multiplies two positive numbers*/
 	multiply_first_num(arg1, arg2, &m, s1_len, s2_len);
-	printf("\nsize: %d top: %c end: %c\n", m.size, m.top->data, m.end->data);
-	(void)p_h;
-	multiply_nums(arg1, arg2, &m, p_h, s1_len, s2_len);
-	printf("\nsize: %d top: %c end: %c\n", m.size, m.top->data, m.end->data);
-
-
-	p_h = m.top;
-
-/*	printf("\nsize: %d top: %c end: %c\n", m.size, m.top->data, m.end->data);*/
-(void)i;	
-	while (p_h)
-	{
-		printf("%c", p_h->data);
-		printf("\nmtop: %c\n", m.top->data);
-		p_h = p_h->next;
-	}
-
-	printf("\n");
-	while (m.top != NULL)
-	{
-		p_h = m.top;
-		m.top = m.top->next;
-		free(p_h);
-	}
-
+	multiply_nums(arg1, arg2, &m, s1_len, s2_len);
+	print(&m);
+	free_nodes(&m);
 	return (0);
 }
 
@@ -161,8 +116,11 @@ void _push(char c, Multiply *pm)
 
 /**
  * multiply_first_num - multiply number
- * @
- * @
+ * @arg1: string of numbers
+ * @arg2: string of numbers
+ * @pm: stack pointer to a node
+ * @s1_len: the length of the string 1
+ * @s2_len: the length of the string 2
  */
 
 void multiply_first_num(char *arg1, char *arg2, Multiply *pm,
@@ -172,10 +130,7 @@ void multiply_first_num(char *arg1, char *arg2, Multiply *pm,
 
 	for (i = s1_len - 1; i >= 0; i--)
 	{
-		mul = (((arg2[s2_len - 1]) - '0') * (arg1[i] - '0'));
-
-		mul += remainder;
-
+		mul = (((arg2[s2_len - 1]) - '0') * (arg1[i] - '0')) + remainder;
 		remainder = 0;
 
 		if (mul <= 9)
@@ -194,87 +149,86 @@ void multiply_first_num(char *arg1, char *arg2, Multiply *pm,
 
 /**
  * multiply_nums - multiply numbers
- * @
- * @
+ * @arg1: string of numbers
+ * @string of numbers
+ * @pm: stack pointer to a node
+ * @s1_len: the length of the string 1
+ * @s2_len: the length of the string 2
  */
 
 void multiply_nums(char *arg1, char *arg2, Multiply *pm,
-	MultiplyNode *p_h, int s1_len, int s2_len)
+	int s1_len, int s2_len)
 {
 	int i, j, z, mul, remainder = 0;
-	int zero = 1, sum = 0;
-	MultiplyNode *p_helper = NULL;
+	int zero = 1, data = 0;
+	MultiplyNode *p_h = NULL;
 
 	p_h = pm->end;
-	for (i = s2_len - 2; i >=0; i--)
+	for (i = s2_len - 2; i >= 0; i--)
 	{
-		printf("\nzero: %d\n", zero);
 		for (z = 0; z < zero; z++)
-		{
-			printf("\nph_pefore: %c\n", p_h->data);
 			p_h = p_h->prev;
-			printf("\nph_after: %c\n", p_h->data);
-		}
-		sum = (p_h->data - '0');
-
 
 		for (j = s1_len - 1; j >= 0; j--)
 		{
-			mul = ((arg2[i] - '0') * (arg1[j] - '0'));
+			data = (p_h->data - '0');
+			mul = ((arg2[i] - '0') * (arg1[j] - '0')) + data + remainder;
+			remainder = 0;
 
-
-			sum += mul;
-			printf("\nph_pefore: %c sum: %d, mul: %d\n", p_h->data, sum, mul);
-
-
-			if (sum <= 9)
+			if (mul <= 9)
 			{
-				p_h->data = (sum + '0');
+				p_h->data = (mul + '0');
 				p_h = p_h->prev;
-				sum = 0;
 			}
 			else
 			{
-				p_h->data = ((sum % 10) + '0');
-				remainder = sum / 10;
+				p_h->data = ((mul % 10) + '0');
+				remainder = mul / 10;
 				p_h = p_h->prev;
-				p_helper = p_h;
-
-				while ((p_h >= pm->top && remainder > 0))
-				{
-					p_h = p_helper;
-					sum = ((p_h->data) - '0') + remainder;
-					remainder = 0;
-
-					if (sum <= 9)
-					{
-						p_h->data = (sum + '0');
-						break;
-					}
-					else
-					{
-						p_h->data = ((sum % 10) + '0');
-						remainder = sum / 10;
-
-						if (p_h)
-							p_h = p_h->prev;
-					}
-				}
-
-				if (remainder > 0)
-				{
-					_push((remainder + '0'), pm);
-					remainder = 0;
-				}
-
-			p_helper = p_helper->prev;
-			p_h = p_helper;
-
 			}
+		}
+		if (remainder > 0)
+		{
+			_push((remainder + '0'), pm);
+			remainder = 0;
 		}
 		zero++;
 		p_h = pm->end;
-		printf("\n\n==========================\n\n");
+	}
+}
+
+/**
+ * print - print linked list
+ * @pm: stack
+ */
+
+void print(Multiply *pm)
+{
+	MultiplyNode *p_h = pm->top;
+
+	while (p_h)
+	{
+		printf("%c", p_h->data);
+		p_h = p_h->next;
+	}
+
+	printf("\n");
+}
+
+/**
+ * free_nodes - free the linked list
+ * @pm: stack
+ */
+
+void free_nodes(Multiply *pm)
+{
+	MultiplyNode *p_h;
+
+	while (pm->top)
+	{
+		p_h = pm->top;
+		pm->top = pm->top->next;
+		free(p_h);
 	}
 }
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,52 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct multiplynode - Struct Node
+ * @data: the element in the node
+ * @next: pointer to the next node
+ * @prev: pointer to the previous node
+ *
+ * Description: node to multiply numbers
+ */
+
+typedef struct multiplynode
+{
+	char data;
+	struct multiplynode *next;
+	struct multiplynode *prev;
+} MultiplyNode;
+
+/**
+ * struct multiply - stack
+ * @size: the size of the nodes
+ * @top: the first node to pop
+ * @end: the last node to pop
+ *
+ * Description: stack pointer to nodes
+ */
+
+typedef struct myltiply
+{
+	int size;
+	MultiplyNode *top;
+	MultiplyNode *end;
+} Multiply;
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _isdigit(char *s);
+void create_node(Multiply *pm);
+void _push(char c, Multiply *pm);
+void multiply_nums(char *arg1, char *arg2,
+	Multiply *pm, int s1_len, int s2_len);
+void multiply_first_num(char *arg1, char *arg2, Multiply *pm,
+int s1_len, int s2_len);
+void print(Multiply *pm);
+void free_nodes(Multiply *pm);
 
+#endif /* #ifndef MAIN_H */
