9e25cc1cf491fb1a77145e43d45b801b45eb7932
MahmoudEasa <mu01011422865@gmail.com>
2023-04-27 08:36:55 -0700

    Fix betty 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
+ * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to first element in list
  * @str: string
  *
