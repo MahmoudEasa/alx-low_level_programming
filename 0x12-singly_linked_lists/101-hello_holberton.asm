dc552c851c53c5fa27c6264a4db38ea01a4ebec2
MahmoudEasa <mu01011422865@gmail.com>
2023-04-27 10:27:53 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+extern printf
+
+section .data
+	msg db "Hello, Holberton", 0
+	format db "%s", 10, 0
+
+section .text
+	global main
+
+main:
+	push rbp
+
+	mov rdi, format
+	mov rsi, msg
+	mov rax, 0
+	call printf
+
+	pop rbp
+
+	mov rax, 0
+	ret
+
