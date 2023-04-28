424a1722127af8ae42d40e5ad5dd4b0662cc72ed
MahmoudEasa <mu01011422865@gmail.com>
2023-04-28 09:38:34 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,5 +32,6 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 listint_t *insert(listint_t *ptr, unsigned int index, int n);
+void delete(listint_t *head);
 
 #endif /* #ifndef LISTS_H */
