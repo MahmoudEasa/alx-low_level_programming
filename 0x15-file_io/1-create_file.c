31bb73897cf36583f0bba159c873118e90675c3e
MahmoudEasa <mu01011422865@gmail.com>
2023-05-08 13:00:01 -0700

    Fix 1-create_file.c 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	f = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 		if (f == -1)
 			return (-1);
--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -18,6 +18,15 @@ int main(int ac, char **av)
     }
     res = create_file(av[1], av[2]);
     printf("-> %i)\n", res);
+
+/*    printf("--------------------- av[1] NULL --------------------\n");
+    res = create_file(av[1], NULL);
+    printf("-> %i)\n", res);*/
+
+/*    printf("--------------------- NULL av[2] --------------------\n");
+    res = create_file(NULL, av[2]);
+    printf("-> %i)\n", res);*/
+
     return (0);
 }
 
