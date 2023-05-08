fecdc83c1059b9229915503d99c457fac87fc3f0
MahmoudEasa <mu01011422865@gmail.com>
2023-05-08 11:33:50 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and prints it to
+ *	the POSIX standard output
+ * @filename: string
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char buffer[1024];
+	int f;
+	ssize_t count;
+
+	if (!filename)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+
+		if (f == -1)
+			return (0);
+
+		count = read(f, buffer, letters);
+
+		if (count == -1)
+			return (0);
+
+		printf("%s", buffer);
+
+	close(f);
+
+	if (count == -1)
+		return (0);
+	else
+		return (count);
+}
+
