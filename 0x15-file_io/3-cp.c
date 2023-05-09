9cfdd464d2277af7392e50e3412d581b067ed2d6
MahmoudEasa <mu01011422865@gmail.com>
2023-05-08 18:15:01 -0700

    Fix 3-cp.c 1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@
 
 int main(int ac, char **av)
 {
-	int f_from, f_to, count;
+	int f_from, f_to, count, w;
 	char *buffer;
 
 	if (ac != 3)
@@ -25,29 +25,31 @@ int main(int ac, char **av)
 	if (!buffer)
 		print_err("Error: Can't write to", av[2], 99, NULL);
 
+	f_from = open(av[1], O_RDONLY);
+		if (f_from == -1)
+			print_err("Error: Can't read from file", av[1], 98, buffer);
 	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	count = read(f_from, buffer, 1024);
 
-		if (f_to == -1)
-			print_err("Error: Can't write to", av[2], 99, buffer);
+		do {
+			if (f_to == -1 || w == -1)
+				print_err("Error: Can't write to", av[2], 99, buffer);
 
-		f_from = open(av[1], O_RDONLY);
-			if (f_from == -1)
-				print_err("Error: Can't read from file", av[1], 98, buffer);
-
-			count = read(f_from, buffer, 1024);
 			if (count == -1)
 				print_err("Error: Can't read from file", av[1], 98, buffer);
 
-		if (close(f_from) == -1)
+			w = write(f_to, buffer, count);
+
+			count = read(f_from, buffer, 1024);
+			f_to = open(av[2], O_WRONLY | O_APPEND);
+		} while (count > 0);
+		if (close(f_to) == -1)
 			print_err("Error:", "Can't close fd -1", 100, buffer);
 
-		count = write(f_to, buffer, strlen(buffer));
-		if (count == -1)
-			print_err("Error: Can't write to", av[2], 99, buffer);
-
-	if (close(f_to) == -1)
+	if (close(f_from) == -1)
 		print_err("Error:", "Can't close fd -1", 100, buffer);
 
+	free(buffer);
 	return (0);
 }
 
