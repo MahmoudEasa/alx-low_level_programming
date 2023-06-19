bc895e0d454d63a41b75f72e444054eb0e68474e
MahmoudEasa <mu01011422865@gmail.com>
2023-06-19 18:29:54 +0300

    Fix 101-make_me_win.sh 1

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o /tmp/librand.so ./rand.c
-export LD_PRELOAD=/tmp/librand.so
+wget -P /tmp/ https://raw.github.com/MahmoudEasa/alx-low_level_programming/master/0x18-dynamic_libraries/librand.so
+LD_PRELOAD=/tmp/librand.so
