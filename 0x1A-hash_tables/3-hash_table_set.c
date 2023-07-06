e8d8049a0aeee917b3a093e6fd8cc55f60b56b29
MahmoudEasa <mu01011422865@gmail.com>
2023-07-07 01:00:32 +0300

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,103 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to the hash table
+ * @key: the key
+ * @value: the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *pos;
+
+	if (key)
+	{
+		index = hash_djb2((unsigned char *)key) % ht->size;
+		pos = ht->array[index];
+
+		found_key(&pos, key, value);
+
+		if (!pos)
+			if (!allocate(ht, index, key, value))
+				return (0);
+	}
+	return (1);
+}
+
+/**
+ * found_key - found key in hash table
+ * @pos: position in hash table
+ * @key: the key
+ * @value: the value
+ */
+
+void found_key(hash_node_t **pos, const char *key, const char *value)
+{
+	while (*pos)
+	{
+		if (strcmp((*pos)->key, key) == 0)
+		{
+			strcpy((*pos)->value, value);
+			break;
+		}
+
+		*pos = (*pos)->next;
+	}
+}
+
+/**
+ * allocate - allocate node
+ * @ht: hash_table_t
+ * @index: the index
+ * @key: the key
+ * @value: the value
+ *
+ * Return: 1 is success or 0
+ */
+
+int allocate(hash_table_t *ht, unsigned long int index,
+		const char *key, const char *value)
+{
+	hash_node_t *new_node, *pos;
+
+	new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	pos = ht->array[index];
+	if (!pos)
+	{
+		ht->array[index] = new_node;
+		pos = ht->array[index];
+	}
+
+	pos->key = (char *) malloc(sizeof(char) * strlen(key) + 1);
+	if (!pos->key)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	pos->value = (char *) malloc(sizeof(char) * (strlen(value) + 1));
+	if (!pos->value)
+	{
+		free(new_node);
+		free(pos->key);
+		return (0);
+	}
+	strcpy(pos->key, key);
+	strcpy(pos->value, value);
+
+	new_node->next = pos;
+	ht->array[index] = new_node;
+	return (1);
+}
+
+
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,6 +37,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+void found_key(hash_node_t **pos, const char *key, const char *value);
+int allocate(hash_table_t *ht, unsigned long int index,
+		const char *key, const char *value);
 
 #endif /* HASH_TABLES */
 
