a031045e3f6bd45bef1305f185b6a24122c6c004
MahmoudEasa <mu01011422865@gmail.com>
2023-10-11 14:50:47 +0000

    Fix 102-interpolation.c 2

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high, pos;
 
-	if (!array || size == 0)
+	if (!array || !size)
 		return (-1);
 
 	high = size - 1;
